c528dc8ce882bdc9b17f0207d3e902af893e1f39
Stefanos Nomidis <sknomidis@gmail.com>
2025-01-12 17:01:11 +0100

    Fixes

--- dsa/algorithms/searching.py
+++ dsa/algorithms/searching.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Protocol, Sequence, TypeVar
+from typing import Protocol, TypeVar
 
 ValueType = TypeVar("ValueType")
 
@@ -12,10 +12,10 @@ class NotFoundError(Exception):
 class SearchAlgorithm(Protocol):
     """Given a sorted array and a value, return the index of the latter."""
 
-    def __call__(self, array: Sequence[ValueType], value: ValueType) -> int: ...
+    def __call__(self, array: list[ValueType], value: ValueType) -> int: ...
 
 
-def linear_search(array: Sequence[ValueType], value: ValueType) -> int:
+def linear_search(array: list[ValueType], value: ValueType) -> int:
     """Linear search algorithm.
 
     Iterate over all elements, until match is found.
@@ -25,11 +25,14 @@ def linear_search(array: Sequence[ValueType], value: ValueType) -> int:
     Time: O(N)
     Space: O(1)
 
-    Comparison
-    ----------
-    + Simple
-    + No sorting required
-    + No extra memory required
+    Pros
+    ----
+    - Simple
+    - No sorting required
+    - No extra memory required
+
+    Cons
+    ----
     - Poor performance
     """
     assert list(array) == sorted(array)
@@ -39,7 +42,7 @@ def linear_search(array: Sequence[ValueType], value: ValueType) -> int:
     raise NotFoundError
 
 
-def binary_search_recursive(array: Sequence[ValueType], value: ValueType) -> int:
+def binary_search_recursive(array: list[ValueType], value: ValueType) -> int:
     """Recursive binary search algorithm.
 
     Recursively halve search interval in sorted array, until value is found.
@@ -49,9 +52,12 @@ def binary_search_recursive(array: Sequence[ValueType], value: ValueType) -> int
     Time: O(log N)
     Space: O(log N)
 
-    Comparison
-    ----------
-    + Fast
+    Pros
+    ----
+    - Fast
+
+    Cons
+    ----
     - Requires sorting
     """
     assert list(array) == sorted(array)
@@ -60,7 +66,7 @@ def binary_search_recursive(array: Sequence[ValueType], value: ValueType) -> int
         if index_start == index_end:
             raise NotFoundError
 
-        index_middle = (index_end + index_start) // 2
+        index_middle = (index_start + index_end) // 2
         if array[index_middle] == value:
             # Value has been found
             return index_middle
@@ -74,7 +80,7 @@ def binary_search_recursive(array: Sequence[ValueType], value: ValueType) -> int
     return search_subarray(index_start=0, index_end=len(array))
 
 
-def binary_search_iterative(array: Sequence[ValueType], value: ValueType) -> int:
+def binary_search_iterative(array: list[ValueType], value: ValueType) -> int:
     """Iterative binary search algorithm.
 
     Recursively halve search interval in sorted array, until value is found.
@@ -84,9 +90,12 @@ def binary_search_iterative(array: Sequence[ValueType], value: ValueType) -> int
     Time: O(log N)
     Space: O(1)
 
-    Comparison
-    ----------
-    + Fast
+    Pros
+    ----
+    - Fast
+
+    Cons
+    ----
     - Requires sorting
     """
     assert list(array) == sorted(array)
@@ -94,18 +103,17 @@ def binary_search_iterative(array: Sequence[ValueType], value: ValueType) -> int
     # Initialization
     index_left = 0
     index_right = len(array)
-    index_middle = (index_left + index_right) // 2
 
-    while value != array[index_middle]:
+    while index_left < index_right:
+        index_middle = (index_left + index_right) // 2
+        if value == array[index_middle]:
+            # Value found
+            return index_middle
+
         if value < array[index_middle]:
-            # Keep looking at left subarray
+            # Search in left subarray
             index_right = index_middle
         else:
-            # Keep looking at right subarray
+            # Search in right subarray
             index_left = index_middle + 1
-        index_middle = (index_left + index_right) // 2
-
-        if index_right == 0 or index_left == len(array):
-            raise NotFoundError
-
-    return index_middle
+    raise NotFoundError
